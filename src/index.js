5d7d4c4b719747b7b5561dac9c3cf91fbde28da1
Yevhen Bashchak <evgeny221@gmail.com>
2023-01-03 20:51:12 +0100

    upd indx

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'globalStyle';
 import { PersistGate } from 'redux-persist/integration/react';
 import { theme } from './theme';
 import { Provider } from 'react-redux';
 import { store, persistor } from 'redux/store';
 import { ChakraProvider } from '@chakra-ui/react';
-import { GlobalStyle } from 'globalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <ChakraProvider>
           <PersistGate loading={null} persistor={persistor}>
             <BrowserRouter>
